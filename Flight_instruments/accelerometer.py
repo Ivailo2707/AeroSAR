8c968a3253adcbb682771917d660686306fe5981
mgr401018 <martin.v.grigorov.2021@elsys-bg.org>
2024-04-01 17:39:22 +0300

    altitude fix

--- Flight_instruments/accelerometer.py
+++ Flight_instruments/accelerometer.py
@@ -2,15 +2,12 @@ import smbus
 import math
 import time
 
-# Define the address of the MPU6050 on the I2C bus
 MPU6050_ADDRESS = 0x68
 
-# MPU6050 registers
 MPU6050_ACCEL_XOUT_H = 0x3B
 MPU6050_ACCEL_YOUT_H = 0x3D
 MPU6050_ACCEL_ZOUT_H = 0x3F
 
-# Initialize I2C bus
 bus = smbus.SMBus(1)
 
 def read_word_2c(address):
@@ -29,9 +26,8 @@ def get_acceleration():
     return x_accel, y_accel, z_accel
 
 def get_speed(acceleration):
-    # Assuming acceleration is in m/s^2, you can use this function to calculate speed
     speed_mps = math.sqrt(acceleration[0]**2 + acceleration[1]**2 + acceleration[2]**2)
-    speed_kph = speed_mps * 3.6  # Convert m/s to km/h
+    speed_kph = speed_mps * 3.6
     return speed_kph
 
 if __name__ == "__main__":
